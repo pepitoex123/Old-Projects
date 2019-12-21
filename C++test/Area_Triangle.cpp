56601622971eb81b2466d0bcae478d94ad743247
pepitoex123 <59104218+pepitoex123@users.noreply.github.com>
2019-12-20 23:12:38 -0300

    Create Area_Triangle.cpp

---
+++ C++test/Area_Triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float b,h,a;
+	cout << "Please enter base and height of the triangle" << endl;
+	cin >> b;
+	cout << endl;
+	cin >> h;
+	cout << endl
+	a = (b*h)/2;
+	cout << "The area of the triangle is " << a << endl;
+	return 0;
+}
